4e0f57671061a68c472609be56f5a7b6d4e91fe5
denn-p <dennielysp@gmail.com>
2026-02-14 00:48:12 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Location
 #from models import Person
 
 app = Flask(__name__)
@@ -38,14 +38,16 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
+    user = User.query.get(1)
+    print(User)
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your GET /user response ",
+        "user": user.serialize() if user else None
     }
 
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,75 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Column, Table
 
 db = SQLAlchemy()
+# le he puesto comentarios a este codigo porque voy a volver en un futuro :)
+favorite_table = Table(
+    "favorite",
+    db.metadata,
+    Column("user_id", ForeignKey("user.id"), primary_key=True),
+    Column("character_id", ForeignKey("character.id"), primary_key=True)
+)
+
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    favorite_characters: Mapped[list["Character"]] = db.relationship(
+        "Character",
+        secondary=favorite_table,
+        # relacion bideccional con character :') se llenan estas tablas.
+        back_populates="favorited_by")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorite_characters": self.favorite_characters
+        }
+
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    image: Mapped[str] = mapped_column(
+        String(500), unique=True, nullable=False)
+    quote: Mapped[str] = mapped_column(String(200), unique=True, nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+#relcion 1-n entre charater y location:
+    location_id: Mapped[int] = mapped_column(ForeignKey("location.id"))  # n
+    location: Mapped["Location"] = relationship(back_populates="characters_location")
+
+#relacion many to many entre character y user:
+favorites_by: Mapped[list[User]] = db.relationship(
+    "User",
+    secondary=favorite_table,# relacion bideccional con user :')))))
+    back_populates="favorite_characters")
+
+
+def serialize(self):
+    return {
+        "id": self.id,
+        "name": self.name,
+        "image": self.image,
+        "quote": self.quote
+    }
+
+
+class Location(db.Model):  # una locacion puede tener varios personajes, pero un personaje solo puede estar en una locacion 1=locacion n=personajes
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), unique=True, nullable=True)
+    character_location: Mapped[list["Character"]] = relationship(
+        "Character", back_populates="location")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "character_location": self.character_location
         }
